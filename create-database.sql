9cec455fcd9a74791f4c969adcdc9774fc105275
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-08-25 01:40:07 +0100

    test answer

--- create-database.sql
+++ create-database.sql
@@ -1,1 +1,14 @@
 -- write your queries here
+
+-- Create a new database called 'SimpleDB'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT name
+        FROM sys.databases
+        WHERE name = N'SimpleDB'
+)
+CREATE DATABASE SimpleDB
+GO
